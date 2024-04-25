e9115e8eac83569f1939ebd1821f79899d709f5a
Anna <anna.mironova89@yandex.ru>
2024-04-25 15:11:29 +0300

    Реализовала работу CLI и добавила команду help

---
+++ src/main.cli.ts
@@ -0,0 +1,11 @@
+import { CLIApplication, HelpCommand } from './cli/index.js';
+
+function bootstrap() {
+  const cliApplication = new CLIApplication();
+  cliApplication.registerCommands([
+    new HelpCommand()
+  ]);
+  cliApplication.processCommand(process.argv);
+}
+
+bootstrap();
