d6c9e3b932d2c3abbb9228578454477c119e1d8a
Anna <anna.mironova89@yandex.ru>
2024-05-06 10:28:55 +0300

    Реализовала логирование с помощью pino

---
+++ src/shared/libs/logger/pino-logger.ts
@@ -0,0 +1,26 @@
+import { pino } from 'pino';
+import { Logger } from './index.js';
+
+export class PinoLogger implements Logger {
+  private readonly logger;
+
+  constructor() {
+    this.logger = pino();
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.warn(message, ...args);
+  }
+
+  public error(message: string, error: Error, ...args: unknown[]): void {
+    this.logger.error(error, message, ...args);
+  }
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug(message, ...args);
+  }
+}
